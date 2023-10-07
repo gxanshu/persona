4cca040c5df9b268556e3ed22f433f9dbb8e7ce0
Anshu <gxanshu@yahoo.com>
2023-10-07 12:26:27 +0530

    search bar fix

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -4,14 +4,15 @@ import { Search } from "~/components/chatpanel/Search";
 
 export default function TestingPage() {
 	return (
-		<div className="p-[72px] flex flex-col max-w-max">
+		<div className="p-[72px]">
+			<div className="flex flex-col border-r max-w-max">
 			<div className="p-[12px]">
 				<ChatHeader/>
 			</div>
 			<div className="p-[12px] border-b">
 				<Search/>
 			</div>
-			<div className="inline-flex flex-col gap-[2px] max-h-[450px] max-w-max overflow-x-hidden overflow-y-auto p-[4px]">
+			<div className="inline-flex flex-col gap-[2px] max-h-[450px] max-w-max overflow-x-hidden overflow-y-auto p-[4px] no-scrollbar">
 				<MsgContainer msg="This is a new message with multi line support, with ellipsis support and here we go"/>
 				<MsgContainer msg="This is a new message with 1 line"/>
 				<MsgContainer msg="This is a new message with 1 line"/>
@@ -23,6 +24,7 @@ export default function TestingPage() {
 				<MsgContainer msg="This is a new message with 1 line"/>
 				<MsgContainer msg="This is a new message with 1 line"/>
 			</div>
+		</div>
 		</div>
 	)
 }
