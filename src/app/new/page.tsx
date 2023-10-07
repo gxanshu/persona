70032396606490c57186a68720d2a5765bc59562
Anshu <gxanshu@yahoo.com>
2023-10-07 12:20:08 +0530

    container

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -4,9 +4,13 @@ import { Search } from "~/components/chatpanel/Search";
 
 export default function TestingPage() {
 	return (
-		<div className="p-[72px] flex flex-col gap-[24px] max-w-max">
-			<ChatHeader/>
-			<Search/>
+		<div className="p-[72px] flex flex-col max-w-max">
+			<div className="p-[12px]">
+				<ChatHeader/>
+			</div>
+			<div className="p-[12px] border-b">
+				<Search/>
+			</div>
 			<div className="inline-flex flex-col gap-[2px] max-h-[450px] max-w-max overflow-x-hidden overflow-y-auto p-[4px]">
 				<MsgContainer msg="This is a new message with multi line support, with ellipsis support and here we go"/>
 				<MsgContainer msg="This is a new message with 1 line"/>
