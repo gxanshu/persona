d4bfb7bd99669a737ce8716cbaf7d380003c8441
anshu <gxanshu@yahoo.com>
2023-10-10 12:10:24 +0530

    done scrolling

--- src/app/profile/components/socialCardList.tsx
+++ src/app/profile/components/socialCardList.tsx
@@ -1,25 +1,26 @@
 'use client';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { SocialList } from './SocialList'
 import { GithubIcon, InstagramIcon, LinkdinIcon, TwitterIcon } from '~/assets/icons'
 export const SocialCardList = () => {
-	const [isScrolling, setIsScrolling] = useState<boolean>(false)
-	let timer:NodeJS.Timeout;
+	const [topHeight, setTopHeight] = useState<number>(0)
+	const [bottomHeight, setBottomHeight] = useState<number>(72)
+	const containerRef = useRef<HTMLDivElement>(null)
 
 	const handleSrolling = () => {
-	if(timer !== null) {
-	        clearTimeout(timer);    
-	    }
-	    setIsScrolling(true)
-	    timer = setTimeout(function() {
-	          setIsScrolling(false)
-	    }, 100);
+		let a = containerRef.current?.scrollTop as number
+		let totalComplete = Math.floor((a/298)*100);
+		let topHeightTobe = Math.floor((totalComplete/100)*72)
+		let bottomHeightTobe = Math.floor((totalComplete/100)*72)
+		// Calculate bottomHeight (opposite direction)
+  let bottomHeightToBe = 72 - topHeightTobe;
+  setBottomHeight(bottomHeightToBe);
+	setTopHeight(topHeightTobe)
 	}
-
 	return (
 		<div className='relative flex max-h-[250px] flex-1 flex-col'>
           <div className="flex flex-1 flex-col overflow-y-auto gap-3 no-scrollbar"
-            onScroll={handleSrolling} >
+            onScroll={handleSrolling} ref={containerRef} >
             <SocialList icon={<TwitterIcon/>} delay={0.2} />
             <SocialList icon={<InstagramIcon/>} delay={0.4}/>
             <SocialList icon={<GithubIcon/>} delay={0.6}/>
@@ -31,8 +32,12 @@ export const SocialCardList = () => {
             <SocialList icon={<TwitterIcon/>} delay={1.8}/>
             <SocialList icon={<TwitterIcon/>} delay={2}/>
           </div>
-          <div className={`h-[30px] pointer-events-none absolute left-[-0.25rem] right-[-0.25rem] top-[-2px] bg-[linear-gradient(180deg,#fff,hsla(0,0%,100%,0))] ${isScrolling ? "opacity-100": "opacity-0"} transition-opacity`}></div>
-          <div className={`h-[42px] pointer-events-none absolute left-[-0.25rem] right-[-0.25rem] bottom-[-2px] bg-[linear-gradient(180deg,hsla(0,0%,100%,0),#fff)] transition-opacity ${isScrolling ? "opacity-100": "opacity-0"}`}></div>
+          <div className={`h-[30px] pointer-events-none absolute left-[-0.25rem] right-[-0.25rem] top-[-2px] bg-[linear-gradient(180deg,#fff,hsla(0,0%,100%,0))]`}
+          style={{height: topHeight}}
+          ></div>
+          <div className={`h-[42px] pointer-events-none absolute left-[-0.25rem] right-[-0.25rem] bottom-[-2px] bg-[linear-gradient(180deg,hsla(0,0%,100%,0),#fff)] transition-opacity`}
+          style={{height: bottomHeight}}
+          ></div>
       </div>
 	)
 }
