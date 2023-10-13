36800b527dbed7147f446cd429bf7dc22e5566f7
anshu <gxanshu@yahoo.com>
2023-10-13 11:27:19 +0530

    footer update

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,20 +1,16 @@
 export default function Footer() {
-	const links = ["Meta", "About", "Blog", "Jobs", "Help", "API", "Privacy", "Terms", "Locations", "Persona Lite", "Threads", "Contact uploading and non-users", "Meta Verfied"]
+	const links = ["About", "Privacy", "Terms" ,"Cookies", "Contact"]
 	return (
 		<footer className="flex flex-col px-[16px] relative align-baseline" role="contentinfo">
-			<div className="overflow-y-visible content-stretch flex flex-col mb-[52px] items-stretch overflow-x-visible relative text-[12px] leading-[12px] text-[#737373] font-sans">
+			<div className="overflow-y-visible content-stretch flex flex-col mb-[24px] items-stretch overflow-x-visible relative text-[12px] leading-[12px] text-[#737373] font-sans">
 				<div className="overflow-visible mt-[24px] flex flex-col items-stretch relative">
 				{/*links area*/}
 				<div className="overflow-visible content-stretch justify-center flex items-stretch flex-wrap ">
+					<p className="mx-[8px] mb-[12px] cursor-default">© BHuman 2023</p>
 					{links.map((link, index) => (
 						<ListItem text={link} key={`link-${index}`} />
 					))}
 				</div>
-				</div>
-				<div className="overflow-visible my-[12px] justify-center flex gap-[16px] items-stretch relative text-[]">
-					{/*meta area*/}
-					<p>English (UK)</p>
-					<p>© 2023 Persona from Bhuman</p>
 				</div>
 			</div>
 		</footer>
