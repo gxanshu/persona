de7b96a56c522d9b562014f3fa4e6f89838b6038
anshu <gxanshu@yahoo.com>
2023-10-27 14:56:18 +0530

    fix second call

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -96,6 +96,7 @@ export default function AiVoiceRecorder() {
     audioContext.suspend().then(() => {
       console.log('AudioContext suspended');
     });
+    setSubtitleChunks([])
   }
     setTime(0)
     console.log('Recorded')
