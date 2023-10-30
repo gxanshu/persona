a37370910dfeef7458fb33b39c5e72c4bb4b1a73
anshu <gxanshu@yahoo.com>
2023-10-30 16:28:58 +0530

    fixed

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -100,6 +100,7 @@ export default function AiVoiceRecorder() {
         })
         //@ts-ignore
         myvad.current.start()
+        mediaRecorder.current?.pause()
 
         connectWebSocket().then(() => {{
           console.log("connectWebSocket connected")}
